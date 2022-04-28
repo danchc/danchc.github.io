d56286ad484012c4b7c0111802ad9d1e044f9de7
Daniel <85527001+danchc@users.noreply.github.com>
2022-04-28 18:16:42 +0200

    files updated
    added dark mode

---
+++ resources/app.js
@@ -0,0 +1,7 @@
+let darkMode = function (icon) {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    icon.classList.toggle('fa-sun');
+  }
+
+//darkMode = (icon) => icon.classList.toggle('fa-sun')
