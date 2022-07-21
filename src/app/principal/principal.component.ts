66a9965a6e4a2d6d1e6953921a60c771950866f5
Daniel <85527001+danchc@users.noreply.github.com>
2022-07-21 21:47:00 +0200

    Updated links

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+
 
 @Component({
   selector: 'principal',
@@ -9,7 +11,14 @@ export class PrincipalComponent implements OnInit {
 
   list = ["Daniel", "Developer"]
 
+  constructor(private viewportScroller: ViewportScroller) {}
+  
   ngOnInit(): void {
+   
   }
+
+  public onClick(elementId: string): void { 
+    this.viewportScroller.scrollToAnchor(elementId);
+}
 
 }
